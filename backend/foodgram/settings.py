9464990498c9e8c961dcb0d44e7146da549ddbae
Георгий Нор <Nor.Junior@ya.ru>
2023-04-29 21:00:09 +0400

    s

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -9,11 +9,12 @@ https://docs.djangoproject.com/en/2.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.2/ref/settings/
 """
-
+from dotenv import load_dotenv
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+load_dotenv()
 
 
 # Quick-start development settings - unsuitable for production
